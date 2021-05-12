e576b4bb3b09539d9e8f6364118f09b1da531e49
Olivia Lewis <ollybaby12@gmail.com>
2021-05-12 16:13:25 -0400

    init deploy prep

--- src/app.js
+++ src/app.js
@@ -7,11 +7,11 @@ const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
+const morganSetting = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
-app.use(morgan(morganOption))
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -36,10 +36,9 @@ app.get('/', (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
     let response
-    if (NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
     } else {
-        console.error(error)
         response = { message: error.message, error }
     }
     res.status(500).json(response)
--- test/app.spec.js
+++ test/app.spec.js
@@ -4,6 +4,7 @@ describe('App', () => {
     it('GET / responds with 200 containing "Welcome to the Fashion Fits API!"', () => {
         return supertest(app)
         .get('/')
+        .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
         .expect(200, 'Welcome to the Fashion Fits API!')
     })
 })
