3589f07a844e932345a2338cee652ee5ac934587
Olivia Lewis <ollybaby12@gmail.com>
2021-05-13 15:28:58 -0400

    add seeds

---
+++ seeds/seed.profileTypes.sql
@@ -0,0 +1,10 @@
+INSERT INTO profile_types (name, bust, waist, hips)
+VALUES
+('Womens', 
+'{"31.5 inches", "33-34 inches", "35.5-37 inches", "39-41 inches", "43-45 inches", "48.5-50.5 inches", "52.5 inches"}',
+'{"24.5 inches", "25.5-27 inches", "28-30 inches", "32-34.5 inches", "37-38.5 inches", "43-45.75 inches", "48.25 inches"}',
+'{"33 inches", "34.5-36 inches", "37.5-39.5 inches", "41.5-43.5 inches", "46-48.5 inches", "51.5-54 inches", "56.75 inches"}'),
+('Mens',
+'{"35 inches", "37 inches", "40 inches", "43 inches", "47 inches", "50 inches", "55 inches"}',
+'{"NA", "NA", "NA", "NA", "NA", "NA", "NA"}',
+'{"NA", "NA", "NA", "NA", "NA", "NA", "NA"}');
---
+++ seeds/seed.profiles.sql
@@ -0,0 +1,17 @@
+INSERT INTO profiles (region_id, profiletype_id, fit, category, number_sizes, results)
+VALUES
+(1, 1, 'USA - Womens', 'Pants, Dresses, Skirts, Coats, and Jackets', '{"0 - 2", "4 - 6", "8 - 10", "12 - 14", "16 - 18", "20 - 22", "24 - 26"}', 'the USA'),
+(1, 2, 'USA - Mens', 'T-shirts, Jackets, and Tank Tops', '{"32", "34 - 36", "38 - 40", "42 - 44", "46", "48", "50"}', 'the USA'),
+(2, 1, 'UK - Womens', 'Pants, Dresses, Skirts, Coats, and Jackets', '{"2 - 4", "6 - 8", "10 -12", "14 - 16", "18 - 20", "22 - 24", "26 - 28"}', 'the UK'),
+(2, 2, 'UK - Mens', 'T-shirts, Jackets, and Tank Tops', '{"32", "34 - 36", "38 - 40", "42 - 44", "46", "48", "50"}', 'the UK'),
+(3, 1, 'Europe - Womens', 'Pants, Dresses, Skirts, Coats, and Jackets', '{"32", "34 - 36", "38 - 40", "42 - 44", "46 - 48", "50 - 52", "54 - 56"}', 'Europe'),
+(3, 2, 'Europe - Mens', 'T-shirts, Jackets, and Tank Tops','{"42", "44 - 46", "48 - 50", "52 - 54", "56", "58", "60"}', 'Europe'),
+(4, 1, 'Japan - Womens', 'Pants, Dresses, Skirts, Coats, and Jackets', '{"5", "7 - 9", "11 - 13", "15 - 17", "19 - 21", "23 - 25", "27 - 29"}', 'Japan'),
+(4, 2, 'Japan - Mens', 'T-shirts, Jackets, and Tank Tops', '{"34", "36 - 38", "40 - 42", "44 - 46", "48", "50", "52"}', 'Japan'),
+(5, 1, 'Italy - Womens', 'Pants, Dresses, Skirts, Coats, and Jackets', '{"38", "40 - 42", "44 - 46", "48 - 50", "52 - 54", "56 - 58", "60"}', 'Italy'),
+(5, 2, 'Italy - Mens', 'T-shirts, Jackets, and Tank Tops', '{"42", "44 - 46", "48 - 50", "52 - 54", "56", "58", "60"}', 'Italy'),
+(6, 1, 'France - Womens', 'Pants, Dresses, Skirts, Coats, and Jackets', '{"34", "36 - 38", "40 - 42", "44 - 46", "48 - 50", "52 - 54", "56"}', 'France'),
+(6, 2, 'France - Mens', 'T-shirts, Jackets, and Tank Tops','{"42", "44 - 46", "48 - 50", "52 - 54", "56", "58", "60"}', 'France'),
+(7, 1, 'Korea - Womens', 'Pants, Dresses, Skirts, Coats, and Jackets', '{"44", "55", "66", "77", "88", "NA", "NA"}', 'Korea'),
+(7, 2, 'Korea - Mens', 'T-shirts, Jackets, and Tank Tops', '{"85", "90 - 95", "100 - 105", "110 - 115", "120", "125", "130"}', 'Korea');
+ 
